fdd69b65e15eb17def0b3466488f727b745baf28
Terehoff <Artem>
2024-04-12 13:20:42 +0700

    Picnic_ready

--- Count_Word.py
+++ Count_Word.py
@@ -1,13 +1,12 @@
 from collections import defaultdict
 
-# Задание 1: подсчет слов
+
 def count_words(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         text = file.read()
         words = text.split()
         return len(words)
 
-# Задание 2: поиск самого длинного слова
 def find_longest_word(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         text = file.read()
@@ -15,7 +14,6 @@ def find_longest_word(filename):
         longest_word = max(words, key=len)
         return longest_word
 
-# Задание 3: вычисление частоты слов
 def word_frequency(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         text = file.read()
